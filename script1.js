c45a71fc6c3f9357609477562561b8a46d68e837
jorgenicol <jorgenicoldagoc18@gmail.com>
2017-01-26 18:17:59 +0800

    First Commit

---
+++ script1.js
@@ -0,0 +1,19 @@
+<script>
+var app = angular.module('toDoApp', []); 
+app.controller('myCtrl', function($scope) {
+    $scope.todoList = [{todoText:'Clean House', done:false}];
+
+    $scope.todoAdd = function() {
+        $scope.todoList.push({todoText:$scope.todoInput, done:false});
+        $scope.todoInput = "";
+    };
+
+    $scope.remove = function() {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.todoList.push(x);
+        });
+    };
+});
+</script>
